91bb3968b66ed12b83454c2d4eb1ffbf9d027478
Serena Chandler <serenachandler2019@gmail.com>
2021-04-14 17:54:08 -0700

    added a fs.write file command to generate the readme

--- index.js
+++ index.js
@@ -2,3 +2,58 @@ const inquirer = require('inquirer');
 
 const fs = require('fs');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'what is the name of your ptoject?',
+      name: 'project',
+    },
+    {
+      type: 'input',
+      message: 'please write a short description of your project.',
+      name: 'description'
+    },
+    {
+      type: 'input',
+      message: "what license should your project have?",
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'what command should be run to install dependencies?',
+      name: "dependencies"
+    },
+    {
+      type: 'input',
+      message: 'what command should be run to run tests?',
+      name: "tests"
+    },
+    {
+      type: 'input',
+      message: 'what does the user need to know about using the repo?',
+      name: "use"
+    },
+    {
+      type: 'input',
+      message: 'what does the user need to know about to contribute to the repo',
+      name: "contribute"
+    },
+
+  ])
+  .then((answers) => {
+    const readmeContent = makeReadme(answers);
+
+    fs.writeFile('readme.md', readmeContent, (err) =>
+    err ? console.log(err) : console.log("generating ReadMe.MD"));
+  });
